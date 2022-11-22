1c9bc18b125a2b3ef897acb5fcd833e2765f0151
EC2 Default User <ec2-user@ip-10-0-7-31.ap-northeast-1.compute.internal>
2022-11-22 03:54:55 +0000

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.json([
+    {
+      id: 1,
+      name: "okuda",
+    },
+    {
+      id: 2,
+      name: "harigaya",
+    },
+  ]);
+});
+
+module.exports = router;
